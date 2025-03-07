61bee228a0eb9a907d2ecd56f63202399a377426
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2025-03-07 15:44:12 +0100

    finetuning app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
-import _ from 'lodash';
+import "./assets/style.css";
 
 function App() {
   const [newSaldo, setNewSaldo] = useState(0);
   const [incomes, setIncomes] = useState([]);
   const [expanses, setExpanses] = useState([]);
-  const [isSortedAsc, setIsSortedAsc] = useState(true);
-  
+  const [isSortedDateAsc, setIsSortedDateAsc] = useState(true);
+  const [isSortedAmountAsc, setIsSortedAmountAsc] = useState(true);
+
   const sortOnDate = (type, list) => {
     let sortedList = [...list].sort((a, b) => {
-      return isSortedAsc ? new Date(a.date) - new Date(b.date) : new Date(b.date) - new Date(a.date) ;
+      return isSortedDateAsc ? new Date(a.date) - new Date(b.date) : new Date(b.date) - new Date(a.date);
     }).reverse();
 
-    if(type === 'expanses'){
+    if (type === 'expanses') {
       setExpanses(sortedList);
-    }else{
+    } else {
       setIncomes(sortedList);
     }
 
-    setIsSortedAsc(!isSortedAsc);
+    setIsSortedDateAsc(!isSortedDateAsc);
+
+    return sortedList;
+  }
+
+  const sortOnAmount = (type, list) => {
+    let sortedList = [...list].sort((a, b) => {
+      return isSortedAmountAsc ? a.money - b.money : b.money - a.money;
+    }).reverse();
+
+    if (type === 'expanses') {
+      setExpanses(sortedList);
+    } else {
+      setIncomes(sortedList);
+    }
+
+    setIsSortedAmountAsc(!isSortedAmountAsc);
 
     return sortedList;
   }
@@ -35,7 +52,7 @@ function App() {
     setIncomes((prevIncomes) => [...prevIncomes, newIncome]);
     event.target.reset();
 
-    setNewSaldo((prevSaldo) => prevSaldo + parseFloat(newIncome.money));
+    setNewSaldo((prevSaldo) => prevSaldo + parseFloat(newIncome.money.replace(",", ".")));
   }
 
   const addExpanse = (event) => {
@@ -47,120 +64,140 @@ function App() {
       "money": event.target.uitmoney.value
     }
 
-    if(newSaldo - parseFloat(newExpanse.money) >= 0){
+    if (newSaldo - parseFloat(newExpanse.money) >= 0) {
       setExpanses((prevExpanses) => [...prevExpanses, newExpanse]);
       event.target.reset();
 
-      setNewSaldo((prevSaldo) => prevSaldo - parseFloat(newExpanse.money));
-    }else{
+      setNewSaldo((prevSaldo) => prevSaldo - parseFloat(newExpanse.money.replace(",", ".")));
+    } else {
       alert('Saldo wordt negatief. Kan deze uitgave niet toevoegen.')
     }
 
-    
+
   }
 
   return (
     <React.Fragment>
-      Vul hier het beginsaldo in: 
-      <form method="POST" onSubmit={(e) => { e.preventDefault(); setNewSaldo(parseFloat(e.target.beginSaldo.value)); e.target.reset(); }}>
-        <input type="text" name="beginSaldo" />
-        <input type="submit" name="submit" value="toevoegen" />
-      </form>
+      <div class="container">
+        <div className="logo">
+          <span>PM</span>Kasboek
+        </div>
 
-      <div className='saldo'>Saldo: {newSaldo}</div>
+        <div className='row'>
+          <div className='col-md-6'>
+            Vul hier het beginsaldo in:
+            <form method="POST" onSubmit={(e) => { 
+              e.preventDefault(); 
+              setNewSaldo(parseFloat(e.target.beginSaldo.value.replace(",", "."))) }}>
+              <input type="text" name="beginSaldo" />
+              <input type="submit" name="submit" value="toevoegen" />
+            </form>
+          </div>
 
-      <div className='inkomsten'>
-        <h3>Inkomsten</h3>
-        <table>
-          <thead>
-            <tr>
-              <th onClick={() => { sortOnDate('incomes', incomes) }}>Datum</th>
-              <th>Beschrijving</th>
-              <th>Bedrag</th>
-            </tr>
-          </thead>
-          <tbody>
-              {incomes.map(item => {
-                return(
+          <div className='col-md-6'>
+            <div className='saldo'>Saldo: &euro; {newSaldo.toString().replace(".", ",")}</div>
+          </div>
+        </div>
+
+
+        <div className='row'>
+          <div className='col-md-6'>
+            <div className='inkomsten'>
+              <h3>Inkomsten</h3>
+
+              <form method="POST" onSubmit={(event) => addIncome(event)}>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Datum</th>
+                      <th>Beschrijving</th>
+                      <th>Bedrag</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><input type='text' name='inkdate' id='inkdate' /></td>
+                      <td><input type='text' name='inkdesc' id='inkdesc' /></td>
+                      <td><input type='text' name='inkmoney' id='inkmoney' /></td>
+                      <td><input type="submit" name="submit" value="toevoegen" /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form>
+
+              <table className="table table-striped">
+                <thead>
                   <tr>
-                    <td>{item.date}</td>
-                    <td>{item.desc}</td>
-                    <td>{item.money}</td>
+                    <th onClick={() => { sortOnDate('incomes', incomes) }}>Datum</th>
+                    <th>Beschrijving</th>
+                    <th onClick={() => { sortOnAmount('incomes', incomes) }}>Bedrag</th>
                   </tr>
-                )
-              })}
-          </tbody>
-        </table>
+                </thead>
+                <tbody>
+                  {incomes.map(item => {
+                    return (
+                      <tr>
+                        <td>{item.date}</td>
+                        <td>{item.desc}</td>
+                        <td>{item.money}</td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
 
-        <form method="POST" onSubmit={(event) => addIncome(event)}>
-          <table>
-            <thead>
-              <tr>
-                <th>Datum</th>
-                <th>Beschrijving</th>
-                <th>Bedrag</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><input type='text' name='inkdate' id='inkdate' /></td>
-                <td><input type='text' name='inkdesc' id='inkdesc' /></td>
-                <td><input type='text' name='inkmoney' id='inkmoney' /></td>
-                <td><input type="submit" name="submit" value="toevoegen" /></td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </div>
+          <div className='col-md-6'>
+            <div className='uitgaven'>
+              <h3>Uitgaven</h3>
 
-      <div className='uitgaven'>
-        <h3>Uitgaven</h3>
+              <form method="POST" onSubmit={(event) => addExpanse(event)}>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Datum</th>
+                      <th>Beschrijving</th>
+                      <th>Bedrag</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><input type='text' name='uitdate' id='uitdate' /></td>
+                      <td><input type='text' name='uitdesc' id='uitdesc' /></td>
+                      <td><input type='text' name='uitmoney' id='uitmoney' /></td>
+                      <td><input type="submit" name="submit" value="toevoegen" /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form>
 
-        <table>
-          <thead>
-            <tr>
-              <th onClick={() => { sortOnDate('expanses', expanses) }}>Datum</th>
-              <th>Beschrijving</th>
-              <th>Bedrag</th>
-            </tr>
-          </thead>
-          <tbody>
-            {expanses.map(item => {
-                return(
+              <table className="table table-striped">
+                <thead>
                   <tr>
-                    <td>{item.date}</td>
-                    <td>{item.desc}</td>
-                    <td>{item.money}</td>
+                    <th onClick={() => { sortOnDate('expanses', expanses) }}>Datum</th>
+                    <th>Beschrijving</th>
+                    <th onClick={() => { sortOnAmount('expanses', expanses) }}>Bedrag</th>
                   </tr>
-                )
-              })}
-          </tbody>
-        </table>
-
-        <form method="POST" onSubmit={(event) => addExpanse(event)}>
-          <table>
-            <thead>
-              <tr>
-                <th>Datum</th>
-                <th>Beschrijving</th>
-                <th>Bedrag</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><input type='text' name='uitdate' id='uitdate' /></td>
-                <td><input type='text' name='uitdesc' id='uitdesc' /></td>
-                <td><input type='text' name='uitmoney' id='uitmoney' /></td>
-                <td><input type="submit" name="submit" value="toevoegen" /></td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
+                </thead>
+                <tbody>
+                  {expanses.map(item => {
+                    return (
+                      <tr>
+                        <td>{item.date}</td>
+                        <td>{item.desc}</td>
+                        <td>{item.money}</td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
-
-
     </React.Fragment>
   )
 }
